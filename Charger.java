f78215ac6e4985224dcd9df854e95e80b231c2d6
Ayush Bahuguna <65734047+ayushbahuguna101@users.noreply.github.com>
2025-01-24 20:48:01 +0530

    Created Charger.java

---
+++ Charger.java
@@ -0,0 +1,11 @@
+class Charger {
+    int id;
+    int chargingRate;
+    int availableHours;
+
+    public Charger(int id, int chargingRate, int availableHours) {
+        this.id = id;
+        this.chargingRate = chargingRate;
+        this.availableHours = availableHours;
+    }
+}
